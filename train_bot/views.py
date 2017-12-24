2e1dc2f4a34f747f5cccdfd7812cd148f9fb55ab
tjtarotu <shinhiro.berg.hitch@gmail.com>
2017-12-24 09:55:11 +0000

    路線情報一覧を画面に表示

--- train_bot/views.py
+++ train_bot/views.py
@@ -6,10 +6,12 @@ from train_bot.models import Railway
 
 uri = "https://api-tokyochallenge.odpt.org/api/v4/odpt:TrainInformation"
 params = {"acl:consumerKey" : "ec9ea47acc38c58335396816f7fd265fb3e68f6aad1546b169005f608529a494"}
+rails = Railway.objects
 
 def index(request):
   train_bot_data = {
       'unko' : unko(),
+      'railway' : rails.all()
       }
   return render(request, 'hello.html', train_bot_data)
 
@@ -23,7 +25,6 @@ def unko():
     return unko_data
 
   unko = json.loads(r.text)
-  rails = Railway.objects
   for u in unko:
     try:
       unko_data[rails.filter(railway=u['odpt:railway'])[0].title] = u['odpt:trainInformationText']
@@ -31,21 +32,3 @@ def unko():
       continue
   return unko_data
 
-def railway():
-  railway_data = {}
-  try:
-    r = requests.get(uri_railway, params = params)
-  except ConnectionRefusedError:
-    return railway_data
-  except requests.exceptions.ConnectionError:
-    return railway_data
-
-  railway = json.loads(r.text)
-
-  for r in railway:
-    try:
-      railway_data[r['owl:sameAs']] = r['dc:title']
-    except KeyError:
-      continue
-
-  return railway_data
