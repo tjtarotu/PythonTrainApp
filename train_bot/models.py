a04d1a346f6634d2dbc35a5be7e0b1ec1f671dd2
tjtarotu <shinhiro.berg.hitch@gmail.com>
2017-12-24 08:08:42 +0000

    モデル初期化用スクリプト作成

--- train_bot/models.py
+++ train_bot/models.py
@@ -7,3 +7,23 @@ class Railway(models.Model):
 
   def __str__(self):
     return self.title
+
+#モデル初期化用mainメソッド
+if __name__=='__main__':
+  import requests
+  import json
+  uri = "https://api-tokyochallenge.odpt.org/api/v4/odpt:Railway"
+  params = {"acl:consumerKey" : "ec9ea47acc38c58335396816f7fd265fb3e68f6aad1546b169005f608529a494"}
+
+  r = requests.get(uri, params=params)
+  railways = json.loads(r.text)
+  
+  for railway in railways:
+    title = railway['dc:title']
+    rail = railway['owl:sameAs']
+    operator = railway['operator']
+    new_railway = Railway(title=title, railway=rail, operator=operator)
+    new_railway.save()
+
+    
+
