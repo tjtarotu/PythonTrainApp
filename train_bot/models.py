0f785e3c6ee4154378cdaebc7c4b787f825e4432
tjtarotu <shinhiro.berg.hitch@gmail.com>
2017-12-24 05:23:28 +0000

    路線モデルの定義

--- train_bot/models.py
+++ train_bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Railway(models.Model):
+  title = models.CharField(max_length = 20)
+  railway = models.CharField(max_length = 100)
+  operator = models.CharField(max_length = 100)
+
+  def __str__(self):
+    return self.title
